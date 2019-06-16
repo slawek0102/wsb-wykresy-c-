3fcec806ba3ee47bb2e4f9e1e64d367b54830e73
Slawek <grzebyk.slawomir@gmail.com>
2019-06-16 11:08:08 +0200

    first commit

---
+++ wykres/wykres/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace wykres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.b = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.c = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chartGraphic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.a = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraphic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b
+            // 
+            this.b.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.b.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.b.Location = new System.Drawing.Point(184, 26);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(61, 23);
+            this.b.TabIndex = 1;
+            this.b.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(245, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.c.Location = new System.Drawing.Point(272, 26);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(61, 23);
+            this.c.TabIndex = 2;
+            this.c.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(28, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "f (x) =";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button1.Location = new System.Drawing.Point(348, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rysuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // chartGraphic
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartGraphic.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartGraphic.Legends.Add(legend1);
+            this.chartGraphic.Location = new System.Drawing.Point(2, 66);
+            this.chartGraphic.Name = "chartGraphic";
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartGraphic.Series.Add(series1);
+            this.chartGraphic.Size = new System.Drawing.Size(581, 493);
+            this.chartGraphic.TabIndex = 4;
+            this.chartGraphic.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(150, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a
+            // 
+            this.a.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.a.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.a.Location = new System.Drawing.Point(88, 28);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(61, 23);
+            this.a.TabIndex = 0;
+            this.a.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "2";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(wykres.Program);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.chartGraphic);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.b);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "Wykres Funkcji";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraphic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox c;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGraphic;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ wykres/wykres/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace wykres
+{
+    public partial class Form1 : Form
+    {
+        
+        static int valA, valB, valC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int calculate(int x)
+        {
+                       
+            return ((valA*x*x)+(valB *x) + valC );
+        }
+        
+            private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try { valA = int.Parse(a.Text); }
+            catch
+            {
+                valA = 0;
+            };
+
+
+            try { valB = int.Parse(b.Text); }
+            catch
+            {
+                valB = 0;
+            };
+
+            try { valC = int.Parse(c.Text); }
+            catch
+            {
+                valC = 0;
+            };
+
+            chartGraphic.Series[0].Points.Clear();
+     
+            // Margin
+            chartGraphic.ChartAreas[0].Position.Auto = false;
+            chartGraphic.ChartAreas[0].Position.X = 10;
+            chartGraphic.ChartAreas[0].Position.Y = 10;
+            chartGraphic.ChartAreas[0].Position.Width = 80;
+            chartGraphic.ChartAreas[0].Position.Height = 80;
+
+            // Both
+            chartGraphic.ChartAreas[0].AxisX.Crossing = 0;
+            chartGraphic.ChartAreas[0].AxisY.Crossing = 0;
+
+            // Axis X
+            chartGraphic.ChartAreas[0].CursorX.IsUserEnabled = false;
+            chartGraphic.ChartAreas[0].CursorX.IsUserSelectionEnabled = false;
+            chartGraphic.ChartAreas[0].AxisX.ScaleView.Zoomable = false;
+            chartGraphic.ChartAreas[0].AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartGraphic.ChartAreas[0].AxisX.LineWidth = 2;
+           
+
+
+            // Axis Y
+            chartGraphic.ChartAreas[0].AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartGraphic.ChartAreas[0].AxisY.LineWidth = 2;
+           
+            
+            
+               // chartGraphic.ChartAreas[0].AxisX.Interval = 50;
+               // chartGraphic.ChartAreas[0].AxisY.Interval = 500;
+            
+
+            for (int i = -29; i <= 31; i++)
+            {
+                chartGraphic.Series[0].Points.AddXY(i, calculate(i));
+                chartGraphic.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        
+    }
+}
